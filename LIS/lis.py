9059fcb2b7dcc7fc006d3928ab7c4c35bd1ffe31
Simion Cartis <simion.cartis1@gmail.com>
2026-08-10 23:42:03 -0400

    Implement lengthOfLIS function in Solution class

---
+++ LIS/lis.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        ans = []
+
+        for num in nums:
+            i = bisect_left(ans,num)
+            if i == len(ans):
+                ans.append(num)
+            else:
+                ans[i] = num
+        return len(ans)
